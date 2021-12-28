1b56a6a63384d65a6225e34a475b28730a98c7ec
oynickj <oynickj@gmail.com>
2021-12-28 13:02:37 -0500

    change title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Josh Oynick'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Josh Oynick'
 
 export default function Layout({ children, home }) {
   return (
