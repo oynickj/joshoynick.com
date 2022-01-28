42600e3f5bf5fa8d2d560ba699b95e363381e12f
oynickj <oynickj@gmail.com>
2022-01-27 21:52:26 -0500

    add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/global.css'
+import '../styles/global.css';
+import {ThemeProvider} from 'next-themes';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+    )
   }
