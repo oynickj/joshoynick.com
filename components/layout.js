b0e45579fb107294b36fe4ed80719cbf1a14b9cc
oynickj <oynickj@gmail.com>
2022-01-28 14:41:55 -0500

    day mode clarification

--- components/layout.js
+++ components/layout.js
@@ -78,7 +78,7 @@ export default function Layout({ children, home }) {
           <button
             onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
           >
-          {theme === 'light' ? '🔅' : '🔆' }
+          {theme === 'light' ? 'Night mode ☾' : 'Day mode 🔆' }
           </button>
         </div>
       </ThemeProvider>
